13d30952830022befd1025ec5363c9149273f97f
Marcus <MarcusMalabag@live.com.ph>
2025-11-28 21:07:20 -0700

    Change Program.cs template

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// <copyright file="Program.cs" company="Jean Deus">
+// Copyright (c) Jean Deus. All rights reserved.
+// </copyright>
+
+/// <summary>
+/// Provides the entry point for the application.
+/// </summary>
+/// <remarks>This class contains the Main method, which is invoked when the application starts. The Main method is
+/// asynchronous and can be used to perform initialization or launch application logic.</remarks>
+public class Program
+{
+    /// <summary>
+    /// Serves as the entry point for the application.
+    /// </summary>
+    /// <param name="args">An array of command-line arguments supplied to the application.</param>
+    /// <returns>A task that represents the asynchronous operation of the application's entry point.</returns>
+    public static async Task Main(string[] args)
+    {
+
+    }
+}
