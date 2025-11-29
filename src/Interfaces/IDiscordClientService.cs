725fce77bfbaf13919003d36eb0d98151790da0b
Marcus <MarcusMalabag@live.com.ph>
2025-11-28 21:08:03 -0700

    Add DiscordServiceClient stubs

---
+++ src/Interfaces/IDiscordClientService.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Program.cs" company="Jean Deus">
+// Copyright (c) Jean Deus. All rights reserved.
+// </copyright>
+
+namespace BihoonBot.App.Interfaces
+{
+    /// <summary>
+    /// Represents a service that provides core functionality for a Discord client implementation.
+    /// </summary>
+    internal interface IDiscordClientService
+    {
+    }
+}
---
+++ src/Services/DiscordClientService.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Program.cs" company="Jean Deus">
+// Copyright (c) Jean Deus. All rights reserved.
+// </copyright>
+
+namespace BihoonBot.App.Services
+{
+
+    /// <summary>
+    /// Provides core functionality for managing a Discord client connection within the application.
+    /// </summary>
+    internal class DiscordClientService
+    {
+    }
+}
