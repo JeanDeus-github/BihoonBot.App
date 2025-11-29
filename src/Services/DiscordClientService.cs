2061f4b098b07b5623b9fe72c14969da1f92f8bb
Marcus <MarcusMalabag@live.com.ph>
2025-11-28 21:59:52 -0700

    Initialize DiscordServiceClientService with DiscordSocketClient

--- src/Services/DiscordClientService.cs
+++ src/Services/DiscordClientService.cs
@@ -4,11 +4,24 @@
 
 namespace BihoonBot.App.Services
 {
+    using Discord.WebSocket;
 
     /// <summary>
     /// Provides core functionality for managing a Discord client connection within the application.
     /// </summary>
     internal class DiscordClientService
     {
+
+        private readonly DiscordSocketClient discordSocketClient;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DiscordClientService"/> class using the specified DiscordSocketClient.
+        /// </summary>
+        /// <param name="discordSocketClient">The DiscordSocketClient instance to be used for managing the connection and interactions with the Discord
+        /// API. Cannot be null.</param>
+        public DiscordClientService(DiscordSocketClient discordSocketClient)
+        {
+            this.discordSocketClient = discordSocketClient;
+        }
+
     }
 }
